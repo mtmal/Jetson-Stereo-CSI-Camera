f2c93b3f2f3b93ad0f22889c048bd6a138d3f482
mtmal <mt@mal>
2023-02-23 22:24:08 +0000

    Fixed indents

--- tests/monoCam_test.cpp
+++ tests/monoCam_test.cpp
@@ -29,17 +29,17 @@
  */
 static void printHelp(const char* name)
 {
-	printf("Usage: %s [options] \n", name);
-	printf("    -h, --help      -> prints this information \n");
-	printf("    -m, --mode      -> sets the specific camera mode, default: 0 \n");
-	printf("    -f, --framerate -> sets the camera framerate in Hz, default: 20 \n");
-	printf("    -c, --cols      -> sets the number of columns (width) in resized image, default: 640 \n");
-	printf("    -r, --rows      -> sets the number of rows (height) in resized image, default: 480 \n");
-	printf("    -i, --id        -> sets the ID of the camera to start, default: 0 \n");
-	printf("\nExample: %s -c 320 -r 240 \n\n", name);
-	printf("NOTE: if the application that uses nvargus to control cameras was killed without releasing the cameras,"
-			" execute the following:\n\n"
-			"$ sudo systemctl restart nvargus-daemon \n\n");
+    printf("Usage: %s [options] \n", name);
+    printf("    -h, --help      -> prints this information \n");
+    printf("    -m, --mode      -> sets the specific camera mode, default: 0 \n");
+    printf("    -f, --framerate -> sets the camera framerate in Hz, default: 20 \n");
+    printf("    -c, --cols      -> sets the number of columns (width) in resized image, default: 640 \n");
+    printf("    -r, --rows      -> sets the number of rows (height) in resized image, default: 480 \n");
+    printf("    -i, --id        -> sets the ID of the camera to start, default: 0 \n");
+    printf("\nExample: %s -c 320 -r 240 \n\n", name);
+    printf("NOTE: if the application that uses nvargus to control cameras was killed without releasing the cameras,"
+        " execute the following:\n\n"
+        "$ sudo systemctl restart nvargus-daemon \n\n");
 }
 
 /**
@@ -51,38 +51,37 @@ static void printHelp(const char* name)
  */
 static void run(const cv::Size& imageSize, const uint8_t framerate, const uint8_t mode, const uint8_t id)
 {
-   /** Current key pressed by the user. */
-   int key = 0;
-   /** The mono camera class. */
-   CSI_Camera camera(imageSize);
-   /** Buffer for image */
-   cv::Mat img;
+    /** Current key pressed by the user. */
+    int key = 0;
+    /** The mono camera class. */
+    CSI_Camera camera(imageSize);
+    /** Buffer for image */
+    cv::Mat img;
 
-   if (camera.startCamera(framerate, mode, id, 0))
-   {
-		cv::namedWindow("CSI Camera", cv::WINDOW_AUTOSIZE);
-		puts("Hit ESC to exit");
+    if (camera.startCamera(framerate, mode, id, 0))
+    {
+        cv::namedWindow("CSI Camera", cv::WINDOW_AUTOSIZE);
+        puts("Hit ESC to exit");
 
-		while (key != 27)
-		{
-			if (camera.getRawImage(img))
-			{
-				cv::imshow("CSI Camera", img);
-			}
-			key = cv::waitKey(30) & 0xff;
-		}
-
-		cv::destroyAllWindows();
-   }
-   else
-   {
-   	puts("Failed to open camera.");
-   }
+        while (key != 27)
+        {
+            if (camera.getRawImage(img))
+            {
+                cv::imshow("CSI Camera", img);
+            }
+            key = cv::waitKey(30) & 0xff;
+        }
+        cv::destroyAllWindows();
+    }
+    else
+    {
+        puts("Failed to open camera.");
+    }
 }
 
 int main(int argc, char** argv)
 {
-	/** Image size for final images (after resize). */
+    /** Image size for final images (after resize). */
     cv::Size imageSize(640, 480);
     /** Framerate at which CSI cameras should capture images. */
     uint8_t framerate = 20;
@@ -99,29 +98,29 @@ int main(int argc, char** argv)
         }
         else if ((0 == strcmp(argv[i], "--framerate")) || (0 == strcmp(argv[i], "-f")))
         {
-        	framerate = static_cast<uint8_t>(atoi(argv[i + 1]));
+            framerate = static_cast<uint8_t>(atoi(argv[i + 1]));
         }
         else if ((0 == strcmp(argv[i], "--cols")) || (0 == strcmp(argv[i], "-c")))
         {
-        	imageSize.width = atoi(argv[i + 1]);
+            imageSize.width = atoi(argv[i + 1]);
         }
         else if ((0 == strcmp(argv[i], "--rows")) || (0 == strcmp(argv[i], "-r")))
         {
-        	imageSize.height = atoi(argv[i + 1]);
+            imageSize.height = atoi(argv[i + 1]);
         }
         else if ((0 == strcmp(argv[i], "--id")) || (0 == strcmp(argv[i], "-i")))
         {
-        	id = static_cast<uint8_t>(atoi(argv[i + 1]));
+            id = static_cast<uint8_t>(atoi(argv[i + 1]));
         }
         else if ((0 == strcmp(argv[i], "--help")) || (0 == strcmp(argv[i], "-h")))
         {
-        	printHelp(argv[0]);
-        	return 0;
+            printHelp(argv[0]);
+            return 0;
         }
-		else
-		{
-			/* nothing to do in here */
-		}
+        else
+        {
+            /* nothing to do in here */
+        }
     }
     run(imageSize, framerate, mode, id);
 
